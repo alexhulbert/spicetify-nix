69eb3fa97e0ad95fc22be833013a3b93907d0996
Tony Nguyen <TonyTheAce@outlook.de>
2022-01-30 20:23:21 +0100

    Update spicetify source

--- spicetify.nix
+++ spicetify.nix
@@ -4,9 +4,9 @@ stdenv.mkDerivation rec {
   name = "spicetify-2.8.3";
 
   src = pkgs.fetchurl {
-    name = "spicetify-2.8.3-linux-amd64.tar.gz";
-    url = https://github.com/khanhas/spicetify-cli/releases/download/v2.8.3/spicetify-2.8.3-linux-amd64.tar.gz;
-    sha256 = "sha256:1zkzajvm0bcsnanmkdab5ma7i7iq3v4qwxghykplxcb9p8dzaayp";
+    name = "spicetify-2.8.5-linux-amd64.tar.gz";
+    url = https://github.com/khanhas/spicetify-cli/releases/download/v2.8.5/spicetify-2.8.5-linux-amd64.tar.gz;
+    sha256 = "sha256:17hshzy9y66ja8syb069dl80icbh0mabx9092cl4h3cgc03v4fnf";
   };
 
   sourceRoot = ".";
